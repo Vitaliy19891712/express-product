84af7d685d9337851bf568a455d6e36728661368
Vitaliy19891712 <stopkaiko@mail.ru>
2023-09-28 19:42:02 +0300

    fix

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const port = process.env.PORT || 3010;
 
 app.use(cors());
 
-app.use(express.static("public"));
+// app.use(express.static("public"));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
